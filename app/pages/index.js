977eccc0176f5e747fc28cf26f953a5569a1612a
Arghya Dutta <arghyadutta119@gmail.com>
2022-12-16 00:41:51 +0530

    Updated wallet button

--- app/pages/index.js
+++ app/pages/index.js
@@ -22,7 +22,7 @@ const Home = () => {
             <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
 
             <div className="button-container">
-                <WalletMultiButton className="cta-button connect-wallet-button" />
+                <WalletMultiButton className="cta-button mint-button" />
             </div>
         </div>
     );
